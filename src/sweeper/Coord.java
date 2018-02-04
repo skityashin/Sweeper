c03c614a776f6ab7acb3f21421697305d5b21879
Skityashin <skityashin@gmail.com>
2018-02-04 23:36:43 +0200

    Rejected commit: Default

--- src/sweeper/Coord.java
+++ src/sweeper/Coord.java
@@ -2,12 +2,22 @@ package sweeper;
 
 
 public class Coord {
+
     public int x;
     public int y;
 
-    public  Coord(int x, int y){
+    public Coord(int x, int y) {
         this.x = x;
         this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Coord) {
+            Coord to = (Coord) obj;
+            return to.x == x && to.y == y;
+        }
+        return super.equals(obj);
     }
 }
 
