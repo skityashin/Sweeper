92ea1938b8d3a17486c021ba67ec2f94e52c503c
Skityashin <skityashin@gmail.com>
2018-02-04 17:30:32 +0200

    Initial commit

---
+++ src/JavaSweeper.java
@@ -0,0 +1,60 @@
+import sweeper.Box;
+import sweeper.Coord;
+
+import javax.swing.*;
+import java.awt.*;
+
+
+public class JavaSweeper extends JFrame {
+
+    private JPanel panel;
+    private final int COLS = 15;
+    private final int ROWS = 1;
+    private final int IMAGE_SIZE = 50;
+
+    public static void main(String[] args) {
+        new JavaSweeper().setVisible(true);
+    }
+
+    private JavaSweeper() {
+        setImages();
+        initPanel();
+        initFrame();
+    }
+
+    private void initPanel() {
+        panel = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                for (Box box : Box.values()) {
+                    Coord coord = new Coord(box.ordinal() * IMAGE_SIZE, 0);
+                    g.drawImage((Image) box.image, coord.x, coord.y, this);
+                }
+            }
+        };
+        panel.setPreferredSize(new Dimension(COLS * IMAGE_SIZE, ROWS * IMAGE_SIZE));
+        add(panel);
+    }
+
+    private void initFrame() {
+        pack();
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Sweeper on Java");
+        setLocationRelativeTo(null);
+        setResizable(false);
+        setVisible(true);
+        setIconImage(getImage("icon"));
+    }
+
+    private void setImages() {
+        for (Box box : Box.values())
+            box.image = getImage(box.name().toLowerCase());
+    }
+
+    private Image getImage(String name) {
+        String filename = "img/" + name.toLowerCase() + ".png";
+        ImageIcon icon = new ImageIcon(getClass().getResource(filename));
+        return icon.getImage();
+    }
+}
